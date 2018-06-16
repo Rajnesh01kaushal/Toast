fc6ab9f2b58169277025fe56eddb0b43a37fb22a
Rajnesh01kaushal <ranjneshkaushal158@gmail.com>
2018-06-16 14:33:43 +0530

    first commit

---
+++ app/src/main/java/com/example/hp/toast/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.hp.toast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "This is my toast in onCreate", Toast.LENGTH_SHORT).show();
+        
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Toast.makeText(this, "This is my toast in onStart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Toast.makeText(this, "This is my toast in onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Toast.makeText(this, "This is my toast in onPause", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Toast.makeText(this, "This is my toast in onStop", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Toast.makeText(this, "This is my toast in onDestroy", Toast.LENGTH_SHORT).show();
+    }
+
+}
